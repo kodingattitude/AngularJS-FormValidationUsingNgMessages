27e8bdc206df30a6397ec2e224dfd87d4a30ba38
kodingattitude <KASANI@DESKTOP-AG1QGVH>
2018-06-06 02:38:41 +0530

    form validation using ng-messages

---
+++ AngularJS-FormValidationUsingNgMessag/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AngularJS_FormValidationUsingNgMessag")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AngularJS_FormValidationUsingNgMessag")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f33f909f-40ef-4ee0-a8ed-af3500284872")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AngularJS-FormValidationUsingNgMessag/index.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module('angularformvalidationusingngmessage', ['ngMessages']);
+app.controller('FormValidationNgMessageController', function ($scope, $window) {
+
+    $scope.SubmitForm = function () {
+        debugger;
+        if ($scope.formvalidation.$valid) {
+            $scope.submitted = false;
+            alert("Submitted!!");
+        }
+        else {
+            //if $scope.formvalidation.$valid is not true then it submitted= true i.e., the submitted attribute should work
+            $scope.submitted = true;
+        }
+
+    }
+
+});
